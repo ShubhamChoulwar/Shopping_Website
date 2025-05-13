4d9743b6ccf56c62f2e6981c1d292b55212121f4
ShubhamChoulwar <choulwarshubham@gmail.com>
2025-05-13 17:01:26 +0530

    create product module

---
+++ creatProduct/src/test/java/com/shopping/site/create/product/creatProduct/CreatProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.site.create.product.creatProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
